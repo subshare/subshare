eee945a729d65e15637b67eb36eb060fe4190951
Marco Nguitragool <marco@codewizards.co>
2014-07-23 17:24:40 +0000

    CC-60

---
+++ org.subshare/org.subshare.rest.client/src/main/java/org/subshare/rest/client/transport/CryptreeRepoTransport.java
@@ -0,0 +1,122 @@
+package org.subshare.rest.client.transport;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
+
+import co.codewizards.cloudstore.core.dto.ChangeSetDTO;
+import co.codewizards.cloudstore.core.dto.RepoFileDTO;
+import co.codewizards.cloudstore.core.dto.RepositoryDTO;
+import co.codewizards.cloudstore.core.repo.transport.AbstractRepoTransport;
+
+public class CryptreeRepoTransport extends AbstractRepoTransport {
+
+	@Override
+	public RepositoryDTO getRepositoryDTO() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public UUID getRepositoryId() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public byte[] getPublicKey() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void requestRepoConnection(final byte[] publicKey) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public ChangeSetDTO getChangeSetDTO(final boolean localSync) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void makeDirectory(final String path, final Date lastModified) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void copy(final String fromPath, final String toPath) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void move(final String fromPath, final String toPath) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void delete(final String path) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public RepoFileDTO getRepoFileDTO(final String path) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public byte[] getFileData(final String path, final long offset, final int length) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void beginPutFile(final String path) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void putFileData(final String path, final long offset, final byte[] fileData) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void endPutFile(final String path, final Date lastModified, final long length, final String sha1) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void endSyncFromRepository() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void endSyncToRepository(final long fromLocalRevision) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	public void makeSymlink(final String path, final String target, final Date lastModified) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+	@Override
+	protected URL determineRemoteRootWithoutPathPrefix() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("NYI");
+	}
+
+}
---
+++ org.subshare/org.subshare.rest.client/src/main/java/org/subshare/rest/client/transport/CryptreeRepoTransportFactory.java
@@ -0,0 +1,39 @@
+package org.subshare.rest.client.transport;
+
+import java.net.URL;
+
+import co.codewizards.cloudstore.core.repo.transport.AbstractRepoTransportFactory;
+import co.codewizards.cloudstore.core.repo.transport.RepoTransport;
+import co.codewizards.cloudstore.rest.client.transport.RestRepoTransportFactory;
+
+public class CryptreeRepoTransportFactory extends AbstractRepoTransportFactory {
+	private final RestRepoTransportFactory restRepoTransportFactory = new RestRepoTransportFactory();
+
+	@Override
+	public int getPriority() {
+		// We replace the ordinary REST repo transport by returning a higher priority.
+		return restRepoTransportFactory.getPriority() + 1;
+	}
+
+	@Override
+	public String getName() {
+		return "SubShare";
+	}
+
+	@Override
+	public String getDescription() {
+		return "SubShare crypto-repository on a remote server accessible via REST";
+	}
+
+	@Override
+	public boolean isSupported(final URL remoteRoot) {
+		// We support the same protocols. Or should we better use a different one like "cc+https",
+		// "cryptree+https" or similar (in analogy to "svn+ssh")?
+		return restRepoTransportFactory.isSupported(remoteRoot);
+	}
+
+	@Override
+	protected RepoTransport _createRepoTransport(final URL remoteRoot) {
+		return new CryptreeRepoTransport();
+	}
+}
