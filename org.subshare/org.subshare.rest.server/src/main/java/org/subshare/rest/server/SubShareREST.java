785940004efd5b8462d6a4821568653c0693511b
Marco Nguitragool <marco@codewizards.co>
2014-07-23 16:50:17 +0000

    CC-58 CC-59

---
+++ org.subshare/org.subshare.rest.server/src/main/java/org/subshare/rest/server/SubShareREST.java
@@ -0,0 +1,25 @@
+package org.subshare.rest.server;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.subshare.rest.server.service.TestSubShareService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import co.codewizards.cloudstore.rest.server.CloudStoreREST;
+
+@ApplicationPath("SubShareREST")
+public class SubShareREST extends CloudStoreREST {
+	private static final Logger logger = LoggerFactory.getLogger(SubShareREST.class);
+
+	static {
+		logger.debug("<static_init>: Class loaded.");
+	}
+
+	{
+		logger.debug("<init>: Instance created.");
+
+		registerClasses(TestSubShareService.class);
+	}
+
+}
---
+++ org.subshare/org.subshare.server/src/main/java/org/subshare/server/SubShareServer.java
@@ -0,0 +1,23 @@
+package org.subshare.server;
+
+import org.subshare.rest.server.SubShareREST;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import co.codewizards.cloudstore.server.CloudStoreServer;
+
+public class SubShareServer extends CloudStoreServer {
+
+	public static void main(final String[] args) throws Exception {
+		setCloudStoreServerClass(SubShareServer.class);
+		CloudStoreServer.main(args);
+	}
+
+	public SubShareServer(final String... args) {
+		super(args);
+	}
+
+	@Override
+	protected ResourceConfig createResourceConfig() {
+		return new SubShareREST();
+	}
+}
