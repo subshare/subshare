215b643af9720d5dca519f3b065a8959dcf0fe0e
Sebastian Schefczyk <arbeit@datenwei.de>
2014-08-28 22:40:24 +0000

    minor: removed deprecated Assert; removed imports from same package.

--- org.subshare/org.subshare.crypto/src/test/java/org/subshare/crypto/MACBenchmark.java
+++ org.subshare/org.subshare.crypto/src/test/java/org/subshare/crypto/MACBenchmark.java
@@ -4,10 +4,7 @@ import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
-import junit.framework.Assert;
-
-import org.subshare.crypto.CryptoRegistry;
-import org.subshare.crypto.MACCalculator;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -54,12 +51,12 @@ public class MACBenchmark
 	public void benchmarkCalculateManyMACsWithSameMACCalculator()
 	throws Exception
 	{
-		byte[] data = new byte[10240 + random.nextInt(4096)];
+		final byte[] data = new byte[10240 + random.nextInt(4096)];
 		random.nextBytes(data);
 
-		MACCalculator macCalculator = CryptoRegistry.getInstance().createMACCalculator(MAC_ALGORITHM, true);
+		final MACCalculator macCalculator = CryptoRegistry.getInstance().createMACCalculator(MAC_ALGORITHM, true);
 		for (int i = 0; i < ITERATION_COUNT; ++i) {
-			byte[] mac = new byte[macCalculator.getMacSize()];
+			final byte[] mac = new byte[macCalculator.getMacSize()];
 			macCalculator.update(data, 0, data.length);
 			macCalculator.doFinal(mac, 0);
 		}
@@ -69,12 +66,12 @@ public class MACBenchmark
 	public void benchmarkCalculateManyMACsWithNewMACCalculator()
 	throws Exception
 	{
-		byte[] data = new byte[10240 + random.nextInt(4096)];
+		final byte[] data = new byte[10240 + random.nextInt(4096)];
 		random.nextBytes(data);
 
 		for (int i = 0; i < ITERATION_COUNT; ++i) {
-			MACCalculator macCalculator = CryptoRegistry.getInstance().createMACCalculator(MAC_ALGORITHM, true);
-			byte[] mac = new byte[macCalculator.getMacSize()];
+			final MACCalculator macCalculator = CryptoRegistry.getInstance().createMACCalculator(MAC_ALGORITHM, true);
+			final byte[] mac = new byte[macCalculator.getMacSize()];
 			macCalculator.update(data, 0, data.length);
 			macCalculator.doFinal(mac, 0);
 		}
@@ -83,20 +80,20 @@ public class MACBenchmark
 	@Test
 	public void testByteOrder()
 	{
-		byte[] ba = new byte[10];
+		final byte[] ba = new byte[10];
 		Arrays.fill(ba, (byte)0);
-		ByteBuffer buf = ByteBuffer.wrap(ba);
+		final ByteBuffer buf = ByteBuffer.wrap(ba);
 
-		int someInt = random.nextInt();
+		final int someInt = random.nextInt();
 
 		ba[0] = (byte)(someInt >>> 8);
 		ba[1] = (byte)someInt;
 
-		short short1 = (short)((ba[0] << 8) + (ba[1] & 0xff));
-		int int1 = ((ba[0] << 8) & 0xffff) + (ba[1] & 0xff);
+		final short short1 = (short)((ba[0] << 8) + (ba[1] & 0xff));
+		final int int1 = ((ba[0] << 8) & 0xffff) + (ba[1] & 0xff);
 
 		buf.rewind();
-		short short2 = buf.getShort();
+		final short short2 = buf.getShort();
 
 		logger.info("short1 = {}", short1);
 		logger.info("short2 = {}", short2);
