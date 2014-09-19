e8b5e65d1bce1672a9fbcb6e0e144e2afe690752
Marco Nguitragool <marco@codewizards.co>
2014-09-19 17:46:27 +0000

    Added some javadoc.

--- org.subshare/org.subshare.crypto/src/main/java/org/subshare/crypto/internal/asymmetric/keypairgenerator/RSAKeyPairGeneratorFactory.java
+++ org.subshare/org.subshare.crypto/src/main/java/org/subshare/crypto/internal/asymmetric/keypairgenerator/RSAKeyPairGeneratorFactory.java
@@ -34,8 +34,27 @@ extends AbstractAsymmetricCipherKeyPairGeneratorFactory
 		setAlgorithmName("RSA");
 	}
 
+	/**
+	 * This value should be a Fermat number. 0x10001 (F4) is current recommended value. 3 (F1) is known to be safe also.
+	 * 3, 5, 17, 257, 65537, 4294967297, 18446744073709551617,
+	 * <p>
+	 * Practically speaking, Windows does not tolerate public exponents which do not fit in a 32-bit unsigned integer.
+	 * Using e=3 or e=65537 works "everywhere".
+	 * <p>
+	 * See: <a href="http://stackoverflow.com/questions/11279595/rsa-public-exponent-defaults-to-65537-what-should-this-value-be-what-are-the">stackoverflow: RSA Public exponent defaults to 65537. ... What are the impacts of my choices?</a>
+	 */
 	private static final BigInteger defaultPublicExponent = BigInteger.valueOf(0x10001);
-	private static final int defaultTests = 12;
+
+	/**
+	 * How certain do we want to be that the chosen primes are really primes.
+	 * <p>
+	 * The higher this number, the more tests are done to make sure they are primes (and not composites).
+	 * <p>
+	 * See: <a href="http://crypto.stackexchange.com/questions/3114/what-is-the-correct-value-for-certainty-in-rsa-key-pair-generation">What is the correct value for “certainty” in RSA key pair generation?</a>
+	 * and
+	 * <a href="http://crypto.stackexchange.com/questions/3126/does-a-high-exponent-compensate-for-a-low-degree-of-certainty?lq=1">Does a high exponent compensate for a low degree of certainty?</a>
+	 */
+	private static final int defaultCertainty = 12;
 
 	private SecureRandom random;
 
@@ -48,7 +67,7 @@ extends AbstractAsymmetricCipherKeyPairGeneratorFactory
 			if (random == null)
 				random = new SecureRandom();
 
-			generator.init(new RSAKeyGenerationParameters(defaultPublicExponent, random, 4096, defaultTests));
+			generator.init(new RSAKeyGenerationParameters(defaultPublicExponent, random, 4096, defaultCertainty));
 		}
 
 		return generator;
