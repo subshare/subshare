3eaa3f5cd2a2e995fe3e1b4e72a5c647c9aeeb59
Marco Nguitragool <marco@codewizards.co>
2015-04-07 15:46:07 +0000

    Added some javadoc.

--- org.subshare/org.subshare.core/src/main/java/org/subshare/core/sign/Signable.java
+++ org.subshare/org.subshare.core/src/main/java/org/subshare/core/sign/Signable.java
@@ -4,6 +4,19 @@ import java.io.InputStream;
 
 public interface Signable {
 
+	/**
+	 * Gets the current version of the signed data, used when signing right now.
+	 * <p>
+	 * If a new field is added to an object implementing {@code Signable} later and this new field's data is
+	 * included in the signature, the value returned by this method must be incremented to avoid breaking all
+	 * old signatures (of all previously signed data).
+	 * <p>
+	 * {@link #getSignedData(int)} then must exclude the newly added field for all version numbers lower than
+	 * the just incremented value.
+	 * <p>
+	 * It is recommended to start with the value 0.
+	 * @return the current version of newly signed data.
+	 */
 	int getSignedDataVersion();
 
 	/**
@@ -12,8 +25,10 @@ public interface Signable {
 	 * Multiple invocations of this method must return the very same bytes, if the object's data was not
 	 * modified.
 	 * @param signedDataVersion version of the signed data. If a new field was added later, it must not be
-	 * contained in the
+	 * contained in the returned {@code InputStream}, if {@code signedDataVersion} is less than the version
+	 * introducing the new field. 
 	 * @return a stream with the object's data to be signed. Never <code>null</code>.
+	 * @see #getSignedDataVersion()
 	 */
 	InputStream getSignedData(int signedDataVersion);
 
