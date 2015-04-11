ef64649378e66751ef579330cb140e25e45ece5d
Marco Nguitragool <marco@codewizards.co>
2015-04-11 09:36:35 +0000

    No caching of GnuPgDir, because not needed (method rarely called, anyway) and causes trouble in tests.

--- org.subshare/org.subshare.core/src/main/java/org/subshare/core/pgp/gnupg/GnuPgDir.java
+++ org.subshare/org.subshare.core/src/main/java/org/subshare/core/pgp/gnupg/GnuPgDir.java
@@ -1,6 +1,6 @@
 package org.subshare.core.pgp.gnupg;
 
-import static co.codewizards.cloudstore.core.oio.OioFileFactory.*;
+import static co.codewizards.cloudstore.core.oio.OioFileFactory.createFile;
 import co.codewizards.cloudstore.core.config.Config;
 import co.codewizards.cloudstore.core.oio.File;
 import co.codewizards.cloudstore.core.util.IOUtil;
@@ -8,8 +8,6 @@ import co.codewizards.cloudstore.core.util.IOUtil;
 public class GnuPgDir {
 
 	public static final String CONFIG_KEY_GNU_PG_DIR = "gnupg.dir";
-
-	private File dir;
 
 	private static final class Holder {
 		public static final GnuPgDir instance = new GnuPgDir();
@@ -22,12 +20,8 @@ public class GnuPgDir {
 	protected GnuPgDir() { }
 
 	public File getFile() {
-		if (dir == null) {
-			final String dirString = Config.getInstance().getProperty(CONFIG_KEY_GNU_PG_DIR, "${user.home}/.gnupg");
-			final String resolvedDir = IOUtil.replaceTemplateVariables(dirString, System.getProperties());
-			dir = createFile(resolvedDir).getAbsoluteFile();
-		}
-		return dir;
+		final String dirString = Config.getInstance().getProperty(CONFIG_KEY_GNU_PG_DIR, "${user.home}/.gnupg");
+		final String resolvedDir = IOUtil.replaceTemplateVariables(dirString, System.getProperties());
+		return createFile(resolvedDir).getAbsoluteFile();
 	}
-
 }
