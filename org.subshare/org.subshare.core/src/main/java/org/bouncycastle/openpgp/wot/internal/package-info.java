62ba3b93f422e5b81ab5129c349641b793ba1069
Marco หงุ่ยตระกูล [Nguitragool] <marco@nightlabs.de>
2017-03-17 21:47:50 +0700

    https://github.com/subshare/subshare/issues/47

--- org.subshare/org.subshare.core/src/main/java/org/bouncycastle/openpgp/wot/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Internal classes used by or even constituting the OpenPGP Web Of Trust (WOT) implementation.
- */
-package org.bouncycastle.openpgp.wot.internal;
--- org.subshare/org.subshare.core/src/main/java/org/bouncycastle/openpgp/wot/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * OpenPGP Web Of Trust (WOT) implementation.
- * <p>
- * The most important class and entry point is {@link org.bouncycastle.openpgp.wot.internal.TrustDbImpl TrustDbImpl}.
- */
-package org.bouncycastle.openpgp.wot;
-
