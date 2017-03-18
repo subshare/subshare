3751f59fe736921eadf930997342e56f5fbbc2bc
Marco หงุ่ยตระกูล [Nguitragool] <marco@nightlabs.de>
2017-03-18 20:44:28 +0700

    https://github.com/subshare/subshare/issues/48
    Fixed broken test.

--- org.subshare/org.subshare.crypto/src/test/java/org/subshare/crypto/GCMTest.java
+++ org.subshare/org.subshare.crypto/src/test/java/org/subshare/crypto/GCMTest.java
@@ -1,87 +1,19 @@
 package org.subshare.crypto;
 
-import org.bouncycastle.crypto.CipherParameters;
+import static org.assertj.core.api.Assertions.*;
+
+import java.security.SecureRandom;
+
 import org.bouncycastle.crypto.engines.TwofishEngine;
 import org.bouncycastle.crypto.modes.AEADBlockCipher;
 import org.bouncycastle.crypto.modes.GCMBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class GCMTest {
 
-//	private static SecureRandom random = new SecureRandom();
-
-//	private static byte[] getBytesFromResource(String resourceName) throws IOException
-//	{
-//		InputStream in = GCMTest.class.getResourceAsStream(resourceName);
-//		ByteArrayOutputStream out = new ByteArrayOutputStream();
-//		IOUtil.transferStreamData(in, out);
-//		in.close();
-//		out.close();
-//		return out.toByteArray();
-//	}
-//
-//	@Test
-//	public void testWithCipher()
-//	throws Exception
-//	{
-//		String debugDumpFileName = "IndexEntryLong_235_20110923_125513_768";
-//
-//		String transformation = "Twofish/GCM/NoPadding";
-////		String transformation = "Twofish/CBC/PKCS5Padding";
-//		Cipher encrypter = CryptoRegistry.getInstance().createCipher(transformation);
-//		Cipher decrypter = CryptoRegistry.getInstance().createCipher(transformation);
-//
-////		byte[] iv = new byte[encrypter.getIVSize()];
-////		random.nextBytes(iv);
-//		byte[] iv = getBytesFromResource(debugDumpFileName + ".iv");
-//
-////		byte[] key = new byte[256 / 8];
-////		random.nextBytes(key);
-//		byte[] key = getBytesFromResource(debugDumpFileName + ".key");
-//
-//		encrypter.init(CipherOperationMode.ENCRYPT, new ParametersWithIV(new KeyParameter(key), iv));
-//		decrypter.init(CipherOperationMode.DECRYPT, new ParametersWithIV(new KeyParameter(key), iv));
-//
-//		byte[] plain = getBytesFromResource(debugDumpFileName + ".plain");
-//		byte[] encrypted = encrypter.doFinal(plain);
-//
-////		byte[] encryptedResource = getBytesFromResource(debugDumpFileName + ".crypt");
-////		Assert.assertArrayEquals("The dumped ciphertext is different from the newly encrypted ciphertext!", encrypted, encryptedResource);
-//
-//		byte[] decrypted = decrypter.doFinal(encrypted);
-//
-//		Assert.assertArrayEquals(plain, decrypted);
-//
-//////		InputStream in = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182710_317");
-////		InputStream in2 = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182714_379");
-//////		InputStream in = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182714_458");
-////		ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-////		IOUtil.transferStreamData(in2, out2);
-////		in2.close();
-////		out2.close();
-////		byte[] plain2 = out2.toByteArray();
-////		byte[] encrypted2 = encrypter.doFinal(plain2);
-////		byte[] decrypted2 = decrypter.doFinal(encrypted2);
-////
-////		Assert.assertArrayEquals(plain2, decrypted2);
-////
-//////		InputStream in = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182710_317");
-//////		InputStream in = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182714_379");
-////		InputStream in3 = GCMTest.class.getResourceAsStream("IndexEntryInteger_6_20110922_182714_458");
-////		ByteArrayOutputStream out3 = new ByteArrayOutputStream();
-////		IOUtil.transferStreamData(in3, out3);
-////		in3.close();
-////		out3.close();
-////		byte[] plain3 = out3.toByteArray();
-////		byte[] encrypted3 = encrypter.doFinal(plain3);
-////		byte[] decrypted3 = decrypter.doFinal(encrypted3);
-////
-////		Assert.assertArrayEquals(plain3, decrypted3);
-//	}
-
+	private static final SecureRandom random = new SecureRandom();
 
 	/*
 	 * There was a problem with GCM encryption. The en/decryption failed with about 1500 objetcs.
@@ -105,22 +37,29 @@ public class GCMTest {
 		byte[] firstCiphertext = null;
 
 		final Cipher cipher = CryptoRegistry.getInstance().createCipher("Twofish/GCM/NoPadding");
+		final Cipher invCipher = CryptoRegistry.getInstance().createCipher("Twofish/GCM/NoPadding");
 //		Cipher cipher = CryptoRegistry.getInstance().createCipher("Twofish/CFB/NoPadding");
 		cipher.init(CipherOperationMode.ENCRYPT, new ParametersWithIV(new KeyParameter(key), iv));
+		invCipher.init(CipherOperationMode.DECRYPT, new ParametersWithIV(new KeyParameter(key), iv));
 
 		for (int i = 0; i < 10000; ++i) {
 			System.out.println("*** " + i + " ***");
+			random.nextBytes(iv);
 //			Thread.sleep(50);
 
 			// Whether we re-initialise with or without key does not matter.
-			cipher.init(CipherOperationMode.ENCRYPT, new ParametersWithIV(null, iv));
 //			cipher.init(CipherOperationMode.ENCRYPT, new ParametersWithIV(new KeyParameter(key), iv));
+			cipher.init(CipherOperationMode.ENCRYPT, new ParametersWithIV(null, iv));
+			invCipher.init(CipherOperationMode.DECRYPT, new ParametersWithIV(null, iv));
 
 			final byte[] ciphertext = cipher.doFinal(plain);
 			if (firstCiphertext == null)
 				firstCiphertext = ciphertext;
 			else
-				Assert.assertArrayEquals(firstCiphertext, ciphertext);
+				assertThat(ciphertext).isNotEqualTo(firstCiphertext);
+
+			byte[] decrypted = invCipher.doFinal(ciphertext);
+			assertThat(decrypted).isEqualTo(plain);
 		}
 	}
 
@@ -138,7 +77,6 @@ public class GCMTest {
 				 1,  2,  3,  4,  5,  6,  7,  8,
 				 9, 10, 11, 12, 13, 14, 15, 16
 		};
-
 
 //		{ // first try CFB - works fine, currently (2011-09-23).
 //			CipherParameters parameters = new ParametersWithIV(new KeyParameter(key), iv);
@@ -166,18 +104,21 @@ public class GCMTest {
 
 
 		{ // now try GCM - fails on 'fhernhache', currently (2011-09-23).
-			final CipherParameters parameters = new ParametersWithIV(new KeyParameter(key), iv);
 			byte[] firstCiphertext = null;
 //			AEADParameters parameters = new AEADParameters(new KeyParameter(key), 128, iv, plain);
 			final AEADBlockCipher cipher = new GCMBlockCipher(new TwofishEngine());
+			final AEADBlockCipher invCipher = new GCMBlockCipher(new TwofishEngine());
 
-			cipher.init(true, parameters);
+			cipher.init(true, new ParametersWithIV(new KeyParameter(key), iv));
+			invCipher.init(false, new ParametersWithIV(new KeyParameter(key), iv));
 
 			for (int i = 0; i < 10000; ++i) {
 				System.out.println("*** gcm " + i + " ***");
+				random.nextBytes(iv);
 
 				// Whether we re-initialise with or without key does not matter.
-				cipher.init(true, parameters);
+				cipher.init(true, new ParametersWithIV(null, iv));
+				invCipher.init(false, new ParametersWithIV(null, iv));
 
 				final byte[] ciphertext = new byte[cipher.getOutputSize(plain.length)];
 				final int encLength = cipher.processBytes(plain, 0, plain.length, ciphertext, 0);
@@ -186,7 +127,14 @@ public class GCMTest {
 				if (firstCiphertext == null)
 					firstCiphertext = ciphertext;
 				else
-					Assert.assertArrayEquals(firstCiphertext, ciphertext);
+					assertThat(ciphertext).isNotEqualTo(firstCiphertext);
+
+				final byte[] decrypted = new byte[cipher.getOutputSize(ciphertext.length)];
+				int decLength = invCipher.processBytes(ciphertext, 0, ciphertext.length, decrypted, 0);
+				decLength += invCipher.doFinal(decrypted, decLength);
+				final byte[] decryptedTruncated = new byte[decLength];
+				System.arraycopy(decrypted, 0, decryptedTruncated, 0, decLength);
+				assertThat(decryptedTruncated).isEqualTo(plain);
 			}
 		}
 	}
