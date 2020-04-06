46951058702b386ebc155486e77d15292287d2c7
Marco Nguitragool <marco@codewizards.co>
2020-04-06 16:44:19 +0200

    Added braces for better readability.

--- org.subshare/org.subshare.core/src/main/java/org/subshare/core/crypto/EncrypterDecrypterStreamUtil.java
+++ org.subshare/org.subshare.core/src/main/java/org/subshare/core/crypto/EncrypterDecrypterStreamUtil.java
@@ -14,15 +14,16 @@ class EncrypterDecrypterStreamUtil {
 	static CipherParameters assertValidKey(final CipherTransformation cipherTransformation, final CipherParameters key) {
 		requireNonNull(cipherTransformation, "cipherTransformation");
 		requireNonNull(key, "key");
-		if (key instanceof KeyParameter)
+		if (key instanceof KeyParameter) {
 			// symmetric encryption
 			if (CryptoKeyType.symmetric != cipherTransformation.getType())
 				throw new IllegalArgumentException("key is a shared secret (used for symmetric encryption), but cipherTransformation is of type: " + cipherTransformation.getType());
-		else if (key instanceof AsymmetricKeyParameter)
+		}
+		else if (key instanceof AsymmetricKeyParameter) {
 			// asymmetric encryption
 			if (CryptoKeyType.asymmetric != cipherTransformation.getType())
 				throw new IllegalArgumentException("key is an asymmetric key, but cipherTransformation is of type: " + cipherTransformation.getType());
-
+		}
 		else
 			throw new IllegalArgumentException("key must be an instanceo of KeyParameter or AsymmetricKeyParameter, but it is an instance of: " + key.getClass().getName());
 
